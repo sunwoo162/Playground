6a78a7bc9bf84c52bbeee895d3a726de71f2b5cc
sunwoo162 <sunwoosunwoo4114@gmail.com>
2026-08-26 21:10:36 +0900

    Expose Luna retrospective runtime to worker

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -7,7 +7,7 @@ mod power_runtime;
 pub mod project_runtime;
 mod reconciliation_runtime;
 pub mod replan_runtime;
-mod retrospective_runtime;
+pub mod retrospective_runtime;
 mod state_runtime;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
