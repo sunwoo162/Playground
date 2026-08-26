ff9a51a1e34a2a080b899df39020a0f53e7321f5
sunwoo162 <sunwoosunwoo4114@gmail.com>
2026-08-26 19:58:58 +0900

    feat: connect character flow to Luna workspace

--- apps/desktop/src/windows/MainWindow.tsx
+++ apps/desktop/src/windows/MainWindow.tsx
@@ -6,6 +6,8 @@ import { FocusPage } from "../pages/FocusPage";
 import { TasksPage } from "../pages/TasksPage";
 import { ActivityPage } from "../pages/ActivityPage";
 import { ToolsPage } from "../pages/ToolsPage";
+import { CharacterPage } from "../pages/CharacterPage";
+import { CharacterCustomizePage } from "../pages/CharacterCustomizePage";
 import { MarketDiscoveryPage } from "../pages/MarketDiscoveryPage";
 import { E2ESmokePage } from "../pages/E2ESmokePage";
 import { ProjectTeamsPage } from "../pages/ProjectTeamsPage";
@@ -14,10 +16,22 @@ import { ShopPage } from "../pages/ShopPage";
 import { SettingsPage } from "../pages/SettingsPage";
 
 export function MainWindow() {
-  const [currentPage, setCurrentPage] = useState<LunaPage>("home");
+  const [currentPage, setCurrentPage] = useState<LunaPage>("character");
 
   const renderPage = () => {
     switch (currentPage) {
+      case "character":
+        return (
+          <CharacterPage
+            onAddCharacter={() => setCurrentPage("character-customize")}
+          />
+        );
+      case "character-customize":
+        return (
+          <CharacterCustomizePage
+            onBack={() => setCurrentPage("character")}
+          />
+        );
       case "focus":
         return <FocusPage />;
       case "tasks":
